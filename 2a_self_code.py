7cc8c6c5d74ad9cc7d6077208a9b8ed4f4a3cb61
AdrianoLandman <e.a.almeidalandman@student.tudelft.nl>
2026-03-06 23:01:42 +0100

    Refactor gradient descent for position and velocity

--- 2a_self_code.py
+++ 2a_self_code.py
@@ -6,7 +6,7 @@ Created on Thu Mar  5 11:08:58 2026
 """
 import numpy as np
 
-#Drone tracking data, respectively x, y and z
+#Drone tracking data for x, y and z
 positions = np.array([
     [2.00, 0.00, 1.00],
     [1.08, 1.68, 2.38],
@@ -18,69 +18,71 @@ positions = np.array([
 
 times = np.array([1, 2, 3, 4, 5, 6])  #Timesteps
 
-#Own gradient descent implementation (similar to example in course site)
-def gradient_descent(initial_guess_v, gradient, learn_rate, max_iter, tol):
+#Own gradient descent implementation (in a similar as shown in the example code)
+def gradient_descent(initial_guess_p0_and_v, gradient, learn_rate, max_iter, tol):
 
-    params = initial_guess_v  #Start from initial guess for velocity
+    params = initial_guess_p0_and_v  #Start from initial guess for p0 and velocity
     
     for iteration in range(max_iter):
-        grad = gradient(params, positions, times)  #Calculate the gradient (slope of error)
+        grad = gradient(params, positions, times)  #Calculate the gradient with gradient of error function defined below
         diff = learn_rate * grad  #Calculate the step size
         
-        if np.linalg.norm(diff) < tol:  #Compare normation of vector (euclidian distance), to tolerance. np.linalf.norm used to make vector into single scalar value
-            break #If step size is small enough, stop
+        if np.linalg.norm(diff) < tol:  #Compare vector (normation of vector) to tolerance. np.linalf.norm used to make vector into single scalar value
+            break #If stepsize is smaller than tolerance than algorithm converges
         
-        params = params - diff  #Update the parameters (velocity)
+        params = params - diff  #If algorithm did not converge, than params is updated and process is repeated
     
     return params
 
 #gradient function assuming initial position is fixed at P(1)
 def gradient_of_error(params, positions, times):  
 
-    vx, vy, vz = params  #Velocity params
-    p0 = positions[0]  #Initial position, which is set at P(t=1)
+    x0, y0, z0, vx, vy, vz = params  
+    p0 = np.array([x0, y0,z0]) #initial position vector
     v = np.array([vx, vy, vz])  #velocity vector
 
-    # Initial gradient vector for velocity
-    dv = np.zeros(3)  # Gradient with respect to velocity
+    #Initial gradient vector for velocity and p0
+    dv = np.zeros(3) 
+    dp0 = np.zeros(3)
 
-    #Loop over each time step to compute the gradients
+    #Compute gradient (loop over each timestep)
     for i in range(len(times)):
-        predicted_position = p0 + v * times[i]  #Predicted position at time t
+        predicted_position = p0 + v * times[i]  
         residual = positions[i] - predicted_position  #residuals
 
-        dv += -2 * times[i] * residual  #Gradient with respect to velocity (so essentially derivative of error function)
+        dp0 += -2 * residual
+        dv += -2 * times[i] * residual  
+        
+    return np.concatenate([dp0, dv])  
 
-    return dv  
 
-
-#Below gradient descent is run to find the 'optimal' constant velocity (initial position is fixed at P(1))
+#run gradient descent below
 optimized_constant_velocity = gradient_descent(
-    initial_guess_v= np.zeros(3),                    # Starting guess for velocity
-    gradient=gradient_of_error, #Gradient function
-    learn_rate=0.001,         # Learning rate
-    max_iter=20000,          # Maximum number of iterations
-    tol=0.001                      # Tolerance for stopping
+    initial_guess_p0_and_v= np.zeros(6),       #Starting guess for velocity
+    gradient=gradient_of_error,                #Gradient function
+    learn_rate=0.0001,                         #LEARNING RATE, CHANGE VALUE TO TUNE LEARNING RATE
+    max_iter=200000,                           #MAX. NUMBER OF ITER., CHANGE VALUE TO TUNE MAX NUMBER OF ITERATIONS
+    tol=0.000001                               #TOLERANCE, CHANGE VALUE TO TUNE TOLERANCE
 )
 
 #the constant velocity
-vx, vy, vz = optimized_constant_velocity
+x0, y0, z0, vx, vy, vz = optimized_constant_velocity
 
 print('Constant velocity for, respectively, vx, vy and vz:', vx, vy, vz)
 
-#Error function assuming initial position is fixed at P(1)
+#Error function, 
 def error_function(params, positions, times):
   
-    vx, vy, vz = params  #Only velocity to estimate
-    p0 = positions[0]  #Initial position is now the position at t = 1
-    v = np.array([params])  #Velocity vector
+    x0, y0, z0, vx, vy, vz = params  
+    p0 = np.array([x0, y0, z0])
+    v = np.array([vx, vy, vz])  
 
     #Predicted positions array with the same dimensions as positions array
     predicted_positions = np.zeros_like(positions)
 
     #Loop over each time step to calculate the predicted position
     for i in range(len(times)):
-        predicted_positions[i] = p0 + v * times[i]  #Predicted position at time t = P(1) + v * t
+        predicted_positions[i] = p0 + v * times[i]  #Predicted position at time t = p0 + v * t
 
     #Sum of squared errors (SSE) between actual and predicted positions
     error = np.sum((positions - predicted_positions) ** 2)
